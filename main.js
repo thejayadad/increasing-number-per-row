eb3b5d06c8680f14850880837677fdfdc7f85e35
jaydunb12 <74875595+jaydunb12@users.noreply.github.com>
2022-08-14 08:34:29 -0400

    update

---
+++ main.js
@@ -0,0 +1,11 @@
+
+let rows = 5;
+for (let i = 1; i <=rows; i++){
+    for (let k = 1; k <=(rows -i); k++){
+        document.write("&nbsp;");
+    }
+    for (let j = 1; j <=i; j++){
+        document.write(j);
+    }
+    document.write("<br/>");
+}
